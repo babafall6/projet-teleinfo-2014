80af67b51f19eca5b5181184d9f6f1f43e17baa1
Eric <ericherve@live.fr>
2014-06-25 20:15:00 +0200

    Exercice 3 @awa gaye

---
+++ Exo3GrEric/MainExo3.java
@@ -0,0 +1,28 @@
+import comportementsAnimal.Pigeon;
+import comportementsAnimal.Poisson;
+import comportementsAnimal.Serpent;
+
+
+/*public class MainExo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Pigeon pigeon1=new Pigeon();
+		Poisson poisson1=new Poisson();
+		Serpent serpent1=new Serpent();
+		System.out.println("======le pigeon===");
+		System.out.println("le cri du pigeon est le:"+pigeon1.crier());
+		System.out.println("son espece est :"+pigeon1.manger());
+		System.out.println("son type de reproduction :"+pigeon1.seReproduit());
+		System.out.println("======le poisson===");
+		System.out.println("le cri du poisson est le:"+poisson1.crier());
+		System.out.println("son espece est :"+poisson1.manger());
+		System.out.println("son type de reproduction :"+poisson1.seReproduit());
+		System.out.println("======le serpent===");
+		System.out.println("le cri du serpent est le:"+serpent1.crier());
+		System.out.println("son espece est :"+serpent1.manger());
+		System.out.println("son type de reproduction :"+serpent1.seReproduit());
+
+	}
+
+}*/
---
+++ Exo3GrEric/comportementsAnimal/Pigeon.java
@@ -0,0 +1,30 @@
+package comportementsAnimal;
+
+import lesInterface.Domestique;
+
+public class Pigeon extends Animal implements Domestique{
+
+    
+	public String crier() {
+		
+		return "roucoulement";
+	}
+
+	@Override
+	public String manger() {
+		// TODO Auto-generated method stub
+		return " omnivore";
+	}
+
+	@Override
+	public String seReproduit() {
+		// TODO Auto-generated method stub
+		return "pondre";
+	}
+
+	public String utilite() {
+		// TODO Auto-generated method stub
+		return "nourriture, distraction";
+	}
+
+}
