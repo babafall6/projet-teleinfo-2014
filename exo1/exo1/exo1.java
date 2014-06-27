d7a767b3612c210a7b435f7b154043aaaba3e4f4
root <root@ouangcool.(none)>
2014-06-27 15:47:41 +0000

    Exercice 1 @Geogess

---
+++ exo1/exo1/exo1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Saisie{
+   float tableauNote[];
+	//
+   public saisie(){
+     tableauNote = new float[5];
+   }
+//methode saisi des notes
+   public void SaisieDesNotes() throws NoteNonValideExeption{
+     Scanner sc = new Sacnner(System.in);
+	for (int i = 0; i<5; i++){
+System.out.println("saisi la note"+(i+1) + " : "));
+
+float f = sc.nexFloat();
+if( f<0 || f> 20)
+throw new NoteNonValideException();
+else{
+this.tableauNote[]
+}
+}}
+   public static void main (String[] arg){
+Saisi s= new Saisie ();
+try{
+s.saisieDesNotes();
+}
+catch(Exception)
+   }
+}
